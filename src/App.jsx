68260e353527bcac154bf9fa06617d21d7eac140
Baruti-1 <barutibaruani1@gmail.com>
2023-09-20 15:01:57 +0300

    Create mobile advice component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
 const App = () => {
-  return <main></main>;
+  return (
+    <main className="bg-neutral-DarkBlue min-h-screen flex justify-center items-center">
+      <section className="text-center bg-neutral-DarkGrayishBlue p-8 rounded-lg">
+        {/* mobile */}
+        <div className="max-w-xs md:hidden">
+          <h6 className="text-primary-NeonGreen">ADVICE # 117</h6>
+          <p className="text-primary-LightCyan font-bold pt-6">
+            <q className="text-[24px] font-ManRope">
+              It is easy to sit up and take notice, what's difficult is getting
+              up and taking action.
+            </q>
+          </p>
+          <div className="flex justify-center pt-7">
+            <img src="images/pattern-divider-mobile.svg" alt="" />
+          </div>
+          <div className="flex justify-center relative mt-4">
+            <div className="absolute top-1">
+              <button className="bg-primary-NeonGreen p-5 rounded-[50px]">
+                <img src="images/icon-dice.svg" alt="" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          LightCyan: 'hsl(193, 38%, 86%)',
+          NeonGreen: 'hsl(150, 100%, 66%)',
+        },
+        neutral: {
+          GrayishBlue: 'hsl(217, 19%, 38%)',
+          DarkGrayishBlue: 'hsl(217, 19%, 24%)',
+          DarkBlue: 'hsl(218, 23%, 16%)',
+        },
+      },
+      fontFamily: {
+        ManRope: ['Manrope'],
+      },
+    },
   },
   plugins: [],
 };
