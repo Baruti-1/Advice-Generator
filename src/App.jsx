fc050a72963a11a466fcf937255223edbef55f38
Baruti-1 <barutibaruani1@gmail.com>
2023-09-20 15:04:12 +0300

    Create desktop advice component

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,26 @@ const App = () => {
             </div>
           </div>
         </div>
+        {/* desktop */}
+        <div className="hidden md:block max-w-sm">
+          <h6 className="text-primary-NeonGreen">ADVICE # 117</h6>
+          <p className="text-primary-LightCyan font-bold pt-6">
+            <q className="text-[24px] font-ManRope">
+              It is easy to sit up and take notice, what's difficult is getting
+              up and taking action.
+            </q>
+          </p>
+          <div className="flex justify-center pt-7">
+            <img src="images/pattern-divider-mobile.svg" alt="" />
+          </div>
+          <div className="flex justify-center relative mt-4">
+            <div className="absolute top-1">
+              <button className="bg-primary-NeonGreen p-5 rounded-[50px]">
+                <img src="images/icon-dice.svg" alt="" />
+              </button>
+            </div>
+          </div>
+        </div>
       </section>
     </main>
   );
